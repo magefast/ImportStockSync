ccbad2a91ba2f368c811ed53ff673fb04c9dc19e
a <magefast@gmail.com>
2021-11-02 09:47:03 +0200

    Strekoza ImportStockSync module

---
+++ Console/Sync.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author magefast@gmail.com www.magefast.com
+ * @command php bin/magento sync:stock
+ */
+
+namespace Strekoza\ImportStockSync\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Sync extends Command
+{
+    protected function configure()
+    {
+        $this->setName('sync:stock');
+        $this->setDescription('Update Stock data from Import file');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        die('-w-w-');
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $generatorModel = $objectManager->create('\Strekoza\Feed\Model\Generator');
+
+        $output->writeln("Export Feed... start");
+
+        $generatorModel->runExport();
+
+        $output->writeln("Export Feed... finish");
+
+        unset($feed);
+        unset($generator);
+    }
+}
---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author magefast@gmail.com www.magefast.com
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Strekoza_ImportStockSync',
+    __DIR__
+);
